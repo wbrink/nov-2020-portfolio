26168cf59f4334ebb7317127051ed808e18be470
wbrink <will.brink55@gmail.com>
2020-11-06 16:56:02 -0800

    navbar bug for mobile and working on hero section

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import Navbar from "./Components/Navbar/Navbar" 
+import Hero from "./Components/Hero/Hero";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      {/* <Hero /> */}
     </div>
   );
 }
---
+++ client/src/Components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import styles from "./Hero.module.scss";
+
+const Hero = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+  
+}
+
+export default Hero;
