b1d36e3a27a63bf10209bf5ab1175a2d413af1c1
wbrink <will.brink55@gmail.com>
2020-11-06 23:44:27 -0800

    fixing background-image for smaller screen sizes

--- client/src/Components/Hero/Hero.js
+++ client/src/Components/Hero/Hero.js
@@ -3,9 +3,10 @@ import styles from "./Hero.module.scss";
 
 const Hero = () => {
   return (
-    <div className={styles.hero} style={{zIndex: "-2"}}>
+    <div className={styles.hero}>
       <h1>WILL BRINK</h1>
       <p>FULL STACK WEB DEVELOPER</p>
+      <a href="#" className={styles.link}>View Portfolio</a>
     </div>
   )
   
